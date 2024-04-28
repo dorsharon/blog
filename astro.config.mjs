659b6834d51d829391f0088c6ea949de3255a60f
dorsharon <dor@fuseautotech.com>
2024-04-28 12:31:00 +0300

    Various fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,10 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
 import icon from "astro-icon";
+import lottie from "astro-integration-lottie";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://dorsharon.github.io',
-  integrations: [mdx(), sitemap(), react(),icon()]
+  site: "https://dorsharon.github.io",
+  integrations: [mdx(), sitemap(), react(), icon(), lottie()],
 });
