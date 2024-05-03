6939685417df7d54eeee5a6d496212260b7b818d
dorsharon <dor@fuseautotech.com>
2024-05-03 15:08:46 +0300

    Various fixes

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,12 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
-	type: 'content',
-	// Type-check frontmatter using a schema
+	type: "content",
 	schema: z.object({
 		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
+		subtitle: z.string(),
+		publishDate: z.coerce.date(),
+		image: z.string().optional(),
 	}),
 });
 
