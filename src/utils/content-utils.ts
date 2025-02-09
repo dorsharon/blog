6ccce10d6eba6c4051be7ef05a6fd1951752a966
dorsharon <dor442@gmail.com>
2025-02-09 11:26:37 +0200

    Add draft ability to view through URL

--- src/utils/content-utils.ts
+++ src/utils/content-utils.ts
@@ -2,7 +2,9 @@ import { type CollectionEntry, getCollection } from 'astro:content';
 
 const isDevelopmentMode = import.meta.env.DEV;
 
-export const getSortedPosts = async (options?: { isDraft: boolean }): Promise<CollectionEntry<'posts'>[]> => {
+export const getSortedPosts = async (options?: { isDraft: boolean }): Promise<
+	CollectionEntry<'posts'>[]
+> => {
 	const { isDraft = false } = options ?? {};
 	const allBlogPosts = await getCollection('posts');
 
